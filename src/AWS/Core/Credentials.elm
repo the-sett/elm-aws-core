4c03bc564604d1a79e067edb3098baf0b912e857
Rupert Smith <rupert@thesett.com>
2019-07-24 10:56:52 +0100

    Fixing name aliasing.

--- src/AWS/Core/Credentials.elm
+++ src/AWS/Core/Credentials.elm
@@ -57,10 +57,10 @@ type alias SessionToken =
 {-| Create AWS credentials given an access key and secret key.
 -}
 fromAccessKeys : AccessKeyId -> SecretAccessKey -> Credentials
-fromAccessKeys accessKeyId secretAccessKey =
+fromAccessKeys keyId secretKey =
     Credentials
-        { accessKeyId = accessKeyId
-        , secretAccessKey = secretAccessKey
+        { accessKeyId = keyId
+        , secretAccessKey = secretKey
         , sessionToken = Nothing
         }
 
@@ -75,19 +75,19 @@ setSessionToken token (Credentials creds) =
 {-| Gets the access key.
 -}
 accessKeyId : Credentials -> AccessKeyId
-accessKeyId (Credentials { accessKeyId }) =
-    accessKeyId
+accessKeyId (Credentials creds) =
+    creds.accessKeyId
 
 
 {-| Gets the secret.
 -}
 secretAccessKey : Credentials -> SecretAccessKey
-secretAccessKey (Credentials { secretAccessKey }) =
-    secretAccessKey
+secretAccessKey (Credentials creds) =
+    creds.secretAccessKey
 
 
 {-| Gets the session token.
 -}
 sessionToken : Credentials -> Maybe SessionToken
-sessionToken (Credentials { sessionToken }) =
-    sessionToken
+sessionToken (Credentials creds) =
+    creds.sessionToken
