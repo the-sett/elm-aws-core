39bbde42b77279f39c3bb1b7bc1a05e352071e4d
Rupert Smith <rupert@thesett.com>
2020-01-13 09:54:20 +0000

    Trimmed unused code from Unsigned.

--- src/AWS/Core/Signers/Unsigned.elm
+++ src/AWS/Core/Signers/Unsigned.elm
@@ -1,23 +1,15 @@
-module AWS.Core.Signers.Unsigned exposing (addAuthorization, addSessionToken, algorithm, authorization, credentialScope, filterHeaders, formatPosix, headers, sign, signature, stringToSign)
+module AWS.Core.Signers.Unsigned exposing (filterHeaders, formatPosix, headers, sign)
 
 import AWS.Core.Body exposing (Body, explicitMimetype)
-import AWS.Core.Credentials as Credentials exposing (Credentials)
 import AWS.Core.Request exposing (Unsigned)
 import AWS.Core.Service as Service exposing (Service)
 import AWS.Core.Signers.Canonical exposing (canonical, canonicalPayload, signedHeaders)
-import Crypto.HMAC exposing (sha256)
 import Http
 import Iso8601
 import Json.Decode as Decode
 import Regex
 import Task exposing (Task)
 import Time exposing (Posix)
-import Word.Bytes as Bytes
-import Word.Hex as Hex
-
-
-
--- http://docs.aws.amazon.com/waf/latest/developerguide/authenticating-requests.html
 
 
 sign :
@@ -65,11 +57,6 @@ sign service date req =
         }
 
 
-algorithm : String
-algorithm =
-    "AWS4-HMAC-SHA256"
-
-
 headers : Service -> Posix -> Body -> List ( String, String ) -> List ( String, String )
 headers service date body extraHeaders =
     let
@@ -104,39 +91,6 @@ formatPosix date =
             (\_ -> "")
 
 
-addSessionToken :
-    Credentials
-    -> List ( String, String )
-    -> List ( String, String )
-addSessionToken creds headersList =
-    creds
-        |> Credentials.sessionToken
-        |> Maybe.map
-            (\token ->
-                ( "x-amz-security-token", token ) :: headersList
-            )
-        |> Maybe.withDefault headersList
-
-
-addAuthorization :
-    Service
-    -> Credentials
-    -> Posix
-    -> Unsigned a
-    -> List ( String, String )
-    -> List ( String, String )
-addAuthorization service creds date req headersList =
-    [ ( "Authorization"
-      , authorization creds
-            date
-            service
-            req
-            (headersList |> (::) ( "Host", Service.host service ))
-      )
-    ]
-        |> List.append headersList
-
-
 
 -- Expects headersToRemove to be all lower-case
 
@@ -149,75 +103,3 @@ filterHeaders headersToRemove headersList =
                 not <| List.member (String.toLower head) headersToRemove
     in
     List.filter matches headersList
-
-
-authorization :
-    Credentials
-    -> Posix
-    -> Service
-    -> Unsigned a
-    -> List ( String, String )
-    -> String
-authorization creds date service req rawHeaders =
-    let
-        -- Content-Type & Accept tend to be amended by Http.request
-        filteredHeaders =
-            filterHeaders [ "content-type", "accept" ] rawHeaders
-
-        canon =
-            canonical (Service.signer service) req.method req.path filteredHeaders req.query req.body
-
-        scope =
-            credentialScope date creds service
-    in
-    [ "AWS4-HMAC-SHA256 Credential="
-        ++ Credentials.accessKeyId creds
-        ++ "/"
-        ++ scope
-    , "SignedHeaders="
-        ++ signedHeaders filteredHeaders
-    , "Signature="
-        ++ signature creds service date (stringToSign algorithm date scope canon)
-    ]
-        |> String.join ", "
-
-
-credentialScope : Posix -> Credentials -> Service -> String
-credentialScope date creds service =
-    [ date |> formatPosix |> String.slice 0 8
-    , Service.region service
-    , Service.endpointPrefix service
-    , "aws4_request"
-    ]
-        |> String.join "/"
-
-
-signature : Credentials -> Service -> Posix -> String -> String
-signature creds service date toSign =
-    let
-        digest =
-            \message key ->
-                Crypto.HMAC.digestBytes sha256
-                    key
-                    (Bytes.fromUTF8 message)
-    in
-    creds
-        |> Credentials.secretAccessKey
-        |> (++) "AWS4"
-        |> Bytes.fromUTF8
-        |> digest (formatPosix date |> String.slice 0 8)
-        |> digest (Service.region service)
-        |> digest (Service.endpointPrefix service)
-        |> digest "aws4_request"
-        |> digest toSign
-        |> Hex.fromByteList
-
-
-stringToSign : String -> Posix -> String -> String -> String
-stringToSign alg date scope canon =
-    [ alg
-    , date |> formatPosix
-    , scope
-    , canon
-    ]
-        |> String.join "\n"
