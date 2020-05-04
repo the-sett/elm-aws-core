659e82c12c7dfbde2a985c8182ed71b65531e611
Rupert Smith <rupert@thesett.com>
2020-05-04 21:58:52 +0100

    Completed and documented.

--- src/AWS/Core/KVEncode.elm
+++ src/AWS/Core/KVEncode.elm
@@ -1,44 +1,68 @@
 module AWS.Core.KVEncode exposing
-    ( KVPairs
+    ( KVPairs, KVField
+    , encode
     , int, float, string, bool
-    , dict
-    , field, object
+    , list, dict
+    , field, optional, object
     )
 
 {-| KVEncode provides encoders to turn things into list of `(String, String)`
-which can be used to build headers or query parameters.
+which can be used to build headers or query parameters. The encoding is one that
+is accepted by many AWS services.
+
+When encoding records (or objects) the following scheme is used:
+
+A field with a simple value is encoded like this:
+
+    ( "Field", "value" )
+
+A field with a list of simple values is encoded like this:
+
+    [ ( "Field.member.1", "value1" )
+    , ( "Field.member.2", "value2" )
+    , ...
+    ]
+
+A field with a single object value is encoded like this:
+
+    [ ( "Field.InnerField1", "innerValue1" )
+    , ( "Field.InnerField2", "innerValue2" )
+    , ...
+    ]
+
+A field with a list of object values is encoded like this:
+
+    [ ( "Field.member.1.InnerField1", "innerValue1" )
+    , ( "Field.member.1.InnerField2", "innerValue2" )
+    , ...
+    ]
 
 
-# Sets of KV String tuples.
+# Sets of KV String tuples and their encoder.
 
-@docs KVPairs
+@docs KVPairs, KVField
+@docs encode
 
 
-# Encoders for simple types and dicts.
+# Encoders for simple types and collections.
 
 @docs int, float, string, bool
-@docs dict
+@docs list, dict
 
 
-# Encoders for records with optional fields or nesting.
+# Encoders for objects with optional fields.
 
-@docs field, object
+@docs field, optional, object
 
 -}
 
 import Dict exposing (Dict)
 
 
-{-| Holds pairs of `(String, String)` tuples.
+{-| Describes pairs of `(String, String)` tuples.
 
-A field with a simple list of values:
-Field.member.X
-
-A field with a more complex list of values:
-Field.member.X.InnerField
-
-A field with a single complex value:
-Field.InnerField
+Note that this data structure also allows values on their own with no keys for
+the convenience of keeping this API in a similar shape to JSON encoders.
 
 -}
 type KVPairs
@@ -47,33 +71,35 @@ type KVPairs
     | Object (List ( String, KVPairs ))
 
 
-type Field
+{-| A field of a record to be encoded in `(String, String)` form.
+-}
+type KVField
     = Pair String KVPairs
     | Skip
 
 
-{-| Encodes a String (identity function).
+{-| Encodes a String without a key.
 -}
 string : String -> KVPairs
 string val =
     Val val
 
 
-{-| Encodes an Int as a String.
+{-| Encodes an Int as a String without a key.
 -}
 int : Int -> KVPairs
 int val =
     String.fromInt val |> Val
 
 
-{-| Encodes an Float as a String.
+{-| Encodes a Float as a String without a key.
 -}
 float : Float -> KVPairs
 float val =
     String.fromFloat val |> Val
 
 
-{-| Encodes an Bool as a String ("true" or "false").
+{-| Encodes an Bool as a String ("true" or "false") without a key.
 -}
 bool : Bool -> KVPairs
 bool val =
@@ -84,33 +110,37 @@ bool val =
         "false" |> Val
 
 
+{-| Encodes a list of items as `KVPairs`.
+-}
 list : (a -> KVPairs) -> List a -> KVPairs
 list enc vals =
     List.map enc vals |> ListKVPairs
 
 
-{-| Combines a Dict with a String encoder for its values into a set of `KVPairs`.
+{-| Combines a Dict with an encoder for its values into a set of `KVPairs`.
 -}
 dict : (a -> KVPairs) -> Dict String a -> KVPairs
 dict enc vals =
-    -- Dict.foldr
-    --     (\k v accum -> ( k, enc v ) :: accum)
-    --     []
-    --     vals
-    --     |> Pairs
-    Object []
+    Dict.foldr
+        (\k v accum -> ( k, enc v ) :: accum)
+        []
+        vals
+        |> Object
 
 
 {-| Encodes a pair of `(String, a)` into `KVPairs`.
 -}
-field : (a -> KVPairs) -> ( String, a ) -> Field
+field : (a -> KVPairs) -> ( String, a ) -> KVField
 field enc ( name, val ) =
     Pair name (enc val)
 
 
 {-| Encodes a pair of `(String, Maybe a)` into `KVPairs`.
+
+Values that are `Nothing` are not encoded in the output, they are skipped over.
+
 -}
-optional : (a -> KVPairs) -> ( String, Maybe a ) -> Field
+optional : (a -> KVPairs) -> ( String, Maybe a ) -> KVField
 optional enc ( name, maybeVal ) =
     case maybeVal of
         Nothing ->
@@ -120,7 +150,9 @@ optional enc ( name, maybeVal ) =
             Pair name (enc val)
 
 
-object : List Field -> KVPairs
+{-| Encodes a list of fields as an object.
+-}
+object : List KVField -> KVPairs
 object fields =
     List.foldr
         (\fld accum ->
@@ -136,21 +168,17 @@ object fields =
         |> Object
 
 
+{-| Encodes a set of KV pairs into `List (String, String)` form.
 
--- A field with a simple list of values:
--- Field.member.X
---
--- A field with a more complex list of values:
--- Field.member.X.InnerField
---
--- A field with a single complex value:
--- Field.InnerField
--- Field.InnerField2
---
--- A field with a simple value:
--- Field
+The keys in the output may be compound to describe a path through objects and
+arrays into some deeper data structure. The possible formats are described in
+the module level documentation above.
 
+Note that only `KVPairs` that have keys are encoded by this. Simple values or
+lists do not have keys and will result in `[]` being returned. This only produces
+a non-empty result if the top-level `KVPair` passed in, describes an object.
 
+-}
 encode : KVPairs -> List ( String, String )
 encode kvp =
     let
