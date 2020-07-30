6e805c0de74622ce16851a53ec177bacf781785e
Rupert Smith <rupert@thesett.com>
2020-07-30 10:23:36 +0100

    Comment describing what AWS gives.

--- src/AWS/Error.elm
+++ src/AWS/Error.elm
@@ -1,5 +1,10 @@
 module AWS.Error exposing (..)
 
+-- Decodes from AWS responses like this:
+-- { "__type":"PasswordResetRequiredException"
+-- , "message":"Password reset required for the user"
+-- }
+
 
 type alias AWSError a =
     { error : a
